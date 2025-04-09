8126bdf242b9772659de68022a3baf6c4d4cb988
Jon Carlson <jdcarlso@gs6102dscmcjdca.ndc.nasa.gov>
2025-04-09 13:53:25 -0400

    chore: lint cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
-import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application'
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
 
 // TODO: handle existing filename/path. Don't overwrite it, just add an incrementing number to the end?
 // TODO: using Zod or an existing JSONSchema, validate that the notebook data is valid before attempting to create the notebook
 // TODO: optional open notebook on save?
 
-const JUPYTERLITE_DATABASE = "JupyterLite Storage"
-const JUPYTERLITE_STORE = 'files'
+const JUPYTERLITE_DATABASE = 'JupyterLite Storage';
+const JUPYTERLITE_STORE = 'files';
 
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyter-notebook-from-json-extension:plugin',
   description:
     'An extension from preloading JupyterLite notebooks into a users browser',
   autoStart: true,
-  activate: activatePlugin,
+  activate: activatePlugin
 };
 
 async function activatePlugin(app: JupyterFrontEnd) {
@@ -21,32 +24,44 @@ async function activatePlugin(app: JupyterFrontEnd) {
   );
 
   window.addEventListener('message', async event => {
-    const { data } = event
-    
+    const { data } = event;
+
     if (!data?.type || data.type !== 'load-notebook') {
       // this message is not the right type, ignore it
-      return
+      return;
     }
 
     if (!data.notebook || !data.filename) {
       // TODO: clean up validation, make sure filename is valid (has .ipynb and no directory)
-      throw new Error(`Can't load notebook: either notebook or filename are missing`)
+      throw new Error(
+        "Can't load notebook: either notebook or filename are missing"
+      );
     }
 
     console.log('Load notebook event caught: ', event);
 
-    await saveNotebookToIndexedDB(JUPYTERLITE_DATABASE, JUPYTERLITE_STORE, data.filename, data.notebook)
-    
-    console.log(`Notebook "${data.filename}" saved successfully.`)
+    await saveNotebookToIndexedDB(
+      JUPYTERLITE_DATABASE,
+      JUPYTERLITE_STORE,
+      data.filename,
+      data.notebook
+    );
+
+    console.log(`Notebook "${data.filename}" saved successfully.`);
 
     app.commands.execute('docmanager:open', {
       path: data.filename,
       factory: 'Notebook'
-    })
+    });
   });
 }
 
-function saveNotebookToIndexedDB(dbName: string, storeName: string, key: string, notebookData: any) {
+function saveNotebookToIndexedDB(
+  dbName: string,
+  storeName: string,
+  key: string,
+  notebookData: any
+) {
   return new Promise<void>((resolve, reject) => {
     const request = indexedDB.open(dbName);
 
@@ -56,7 +71,7 @@ function saveNotebookToIndexedDB(dbName: string, storeName: string, key: string,
 
     request.onsuccess = (event: any) => {
       const db = event.target.result;
-      const transaction = db.transaction([storeName], "readwrite");
+      const transaction = db.transaction([storeName], 'readwrite');
       const objectStore = transaction.objectStore(storeName);
       const putRequest = objectStore.put(notebookData, key);
 
